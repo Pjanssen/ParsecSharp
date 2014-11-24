e6a3b2429f42e18b0f1e907d85c57e19baab2420
Pier Janssen <pier@pjanssen.nl>
2014-11-24 21:14:26 +0100

    Added Numeric.Int parser

---
+++ ParsecSharp/Numeric.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PJanssen.ParsecSharp
+{
+   /// <summary>
+   /// Defines Parsers for numeric values.
+   /// </summary>
+   public static class Numeric
+   {
+      /// <summary>
+      /// Parses an integer value.
+      /// </summary>
+      public static Parser<int> Int()
+      {
+         return from x in Combine.Many1(Combine.Or(Chars.Digit(), Chars.Char('-')))
+                select int.Parse(x);
+      }
+   }
+}
