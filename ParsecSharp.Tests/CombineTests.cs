616b2c54a3e65fd6e156951bf03ef772208cb32c
Pier Janssen <pier@pjanssen.nl>
2014-12-14 16:36:31 +0100

    Added Combine.Many, Many1

--- ParsecSharp.Tests/CombineTests.cs
+++ ParsecSharp.Tests/CombineTests.cs
@@ -6,55 +6,6 @@ namespace PJanssen.ParsecSharp
    [TestClass]
    public class CombineTests
    {
-      //#region NotFollowedBy
-
-      //[TestMethod]
-      //public void NotFollowedBy_Success_ReturnsError()
-      //{
-      //   var parser = Combine.NotFollowedBy(Chars.Any());
-      //   var result = parser.Parse("xyz");
-
-      //   ParseAssert.ErrorEquals("Unexpected \"x\"", result);
-      //}
-
-      //[TestMethod]
-      //public void NotFollowedBy_Error_ReturnsSuccess()
-      //{
-      //   var parser = Combine.NotFollowedBy(Chars.String("xyz"));
-      //   var result = parser.Parse("x");
-
-      //   ParseAssert.ValueEquals(Unit.Instance, result);
-      //}
-
-      //[TestMethod]
-      //public void NotFollowedBy_ParserAError_ReturnsError()
-      //{
-      //   var parser = Chars.Char('x').NotFollowedBy(Chars.Char('y'));
-      //   var result = parser.Parse("y");
-
-      //   ParseAssert.IsError(result);
-      //}
-
-      //[TestMethod]
-      //public void NotFollowedBy_ParserBError_ReturnsParserAValue()
-      //{
-      //   var parser = Chars.Char('x').NotFollowedBy(Chars.Char('y'));
-      //   var result = parser.Parse("xz");
-
-      //   ParseAssert.ValueEquals('x', result);
-      //}
-
-      //[TestMethod]
-      //public void NotFollowedBy_ParserBSuccess_ReturnsError()
-      //{
-      //   var parser = Chars.Char('x').NotFollowedBy(Chars.Char('y'));
-      //   var result = parser.Parse("xy");
-
-      //   ParseAssert.IsError(result);
-      //}
-
-      //#endregion
-
       #region Or
 
       [TestMethod]
@@ -91,196 +42,88 @@ namespace PJanssen.ParsecSharp
 
       #endregion
 
-      //#region Choose
+      #region Many
 
-      //[TestMethod]
-      //public void Choose_EmptySequence_ReturnsError()
-      //{
-      //   var parser = Combine.Choose<char>();
-      //   var result = parser.Parse("z");
+      [TestMethod]
+      public void Many_NoMatch_ReturnsEmptySet()
+      {
+         var parser = Chars.Char('x').Many();
+         var result = parser.Parse("y");
 
-      //   ParseAssert.IsError(result);
-      //}
+         ParseAssert.ValueEquals("", result);
+      }
 
-      //[TestMethod]
-      //public void Choose_UnsuccesfulParsers_ReturnsError()
-      //{
-      //   var parser = Combine.Choose(Chars.Char('x'), Chars.Char('y'), Chars.Char('z'));
-      //   var result = parser.Parse("a");
+      [TestMethod]
+      public void Many_OneMatch_ReturnsSetWithOneMatch()
+      {
+         var parser = Chars.Char('x').Many();
+         var result = parser.Parse("xy");
+         
+         ParseAssert.ValueEquals("x", result);
+      }
 
-      //   ParseAssert.IsError(result);
-      //}
+      [TestMethod]
+      public void Many_ManyMatches_ReturnsSet()
+      {
+         var parser = Chars.OneOf("xyz").Many();
+         var result = parser.Parse("xxyz0");
 
-      //[TestMethod]
-      //public void Choose_SuccesfulParsers_ReturnsFirstSuccess()
-      //{
-      //   var parser = Combine.Choose(Chars.Char('x'), Chars.Char('y'), Chars.Char('z'));
-      //   var result = parser.Parse("y");
+         ParseAssert.ValueEquals("xxyz", result);
+      }
 
-      //   ParseAssert.ValueEquals('y', result);
-      //}
+      [TestMethod]
+      public void Many_RunMultipleTimes_ReturnsSameResult()
+      {
+         var parser = Chars.Char('x').Many();
+         var result = parser.Parse("x");
+         result = parser.Parse("x");
 
-      //#endregion
+         ParseAssert.ValueEquals("x", result);
+      }
 
-      //#region Many
+      [TestMethod]
+      public void Many_IEnumerable()
+      {
+         var parser = (from c in Chars.Any() 
+                       from x in Parse.Succeed(42) 
+                       select x).Many();
+         var result = parser.Parse("xyz");
 
-      //[TestMethod]
-      //public void Many_NoMatch_ReturnsEmptySet()
-      //{
-      //   var parser = Combine.Many(Chars.Char('x'));
-      //   var result = parser.Parse("y");
+         ParseAssert.ValueEquals(new[] { 42, 42, 42 }, result);
+      }
 
-      //   ParseAssert.ValueEquals("", result);
-      //}
+      #endregion
 
-      //[TestMethod]
-      //public void Many_OneMatch_ReturnsSetWithOneMatch()
-      //{
-      //   var parser = Combine.Many(Chars.Char('x'));
-      //   var result = parser.Parse("xy");
+      #region Many1
 
-      //   ParseAssert.ValueEquals("x", result);
-      //}
+      [TestMethod]
+      public void Many1_NoMatch_ReturnsError()
+      {
+         var parser = Chars.Char('x').Many1();
+         var result = parser.Parse("y");
 
-      //[TestMethod]
-      //public void Many_ManyMatches_ReturnsSet()
-      //{
-      //   var parser = Combine.Many(Chars.OneOf("xyz"));
-      //   var result = parser.Parse("xxyz0");
+         ParseAssert.IsError(result);
+      }
 
-      //   ParseAssert.ValueEquals("xxyz", result);
-      //}
+      [TestMethod]
+      public void Many1_OneMatch_ReturnsSetWithOneMatch()
+      {
+         var parser = Chars.Char('x').Many1();
+         var result = parser.Parse("xy");
 
-      //[TestMethod]
-      //public void Many_RunMultipleTimes_ReturnsSameResult()
-      //{
-      //   Parser<string> parser = Chars.Char('x').Many();
-      //   var result = parser.Parse("x");
-      //   result = parser.Parse("x");
+         ParseAssert.ValueEquals("x", result);
+      }
 
-      //   ParseAssert.ValueEquals("x", result);
-      //}
+      [TestMethod]
+      public void Many1_ManyMatches_ReturnsSet()
+      {
+         var parser = Chars.OneOf("xyz").Many1();
+         var result = parser.Parse("xxyz");
 
-      //#endregion
+         ParseAssert.ValueEquals("xxyz", result);
+      }
 
-      //#region Many1
+      #endregion
 
-      //[TestMethod]
-      //public void Many1_NoMatch_ReturnsError()
-      //{
-      //   var parser = Combine.Many1(Chars.Char('x'));
-      //   var result = parser.Parse("y");
-
-      //   ParseAssert.IsError(result);
-      //}
-
-      //[TestMethod]
-      //public void Many1_OneMatch_ReturnsSetWithOneMatch()
-      //{
-      //   var parser = Combine.Many1(Chars.Char('x'));
-      //   var result = parser.Parse("xy");
-
-      //   ParseAssert.ValueEquals("x", result);
-      //}
-
-      //[TestMethod]
-      //public void Many1_ManyMatches_ReturnsSet()
-      //{
-      //   var parser = Combine.Many1(Chars.OneOf("xyz"));
-      //   var result = parser.Parse("xxyz");
-
-      //   ParseAssert.ValueEquals("xxyz", result);
-      //}
-
-      //#endregion
-
-      //#region Between
-
-      //[TestMethod]
-      //public void Between_NonMatchingOpen_ReturnsError()
-      //{
-      //   var parser = Chars.Char('x').Between(Chars.Char('['), Chars.Char(']'));
-      //   var result = parser.Parse("_x]");
-
-      //   ParseAssert.IsError(result);
-      //}
-
-      //[TestMethod]
-      //public void Between_NonMatchingClose_ReturnsError()
-      //{
-      //   var parser = Chars.Char('x').Between(Chars.Char('['), Chars.Char(']'));
-      //   var result = parser.Parse("[x_");
-
-      //   ParseAssert.IsError(result);
-      //}
-
-      //[TestMethod]
-      //public void Between_NonMatchingValue_ReturnsError()
-      //{
-      //   var parser = Chars.Char('x').Between(Chars.Char('['), Chars.Char(']'));
-      //   var result = parser.Parse("[_]");
-
-      //   ParseAssert.IsError(result);
-      //}
-
-      //[TestMethod]
-      //public void Between_AllMatching_ReturnsValue()
-      //{
-      //   var parser = Chars.Any().Between(Chars.Char('['), Chars.Char(']'));
-      //   var result = parser.Parse("[x]");
-
-      //   ParseAssert.ValueEquals('x', result);
-      //}
-
-      //[TestMethod]
-      //public void Between_RepeatingValue_ReturnsValue()
-      //{
-      //   var parser = Chars.NoneOf("]").Many().Between(Chars.Char('['), Chars.Char(']'));
-      //   var result = parser.Parse("[xyz]");
-
-      //   ParseAssert.ValueEquals("xyz", result);
-      //}
-
-      //#endregion
-
-      //#region SeparatedBy
-
-      //[TestMethod]
-      //public void SeparatedBy_Nothing_ReturnsEmptyValue()
-      //{
-      //   var parser = Chars.Any().SeparatedBy(Chars.Char(';'));
-      //   var result = parser.Parse("");
-
-      //   ParseAssert.ValueEquals(new char[] { }, result);
-      //}
-
-      //[TestMethod]
-      //public void SeparatedBy_ValueOnly_ReturnsValue()
-      //{
-      //   var parser = Chars.Any().SeparatedBy(Chars.Char(';'));
-      //   var result = parser.Parse("x");
-
-      //   ParseAssert.ValueEquals(new char[] { 'x' }, result);
-      //}
-
-      //[TestMethod]
-      //public void SeparatedBy_ValueAndSeparator_ReturnsError()
-      //{
-      //   var parser = Chars.Any().SeparatedBy(Chars.Char(';'));
-      //   var result = parser.Parse("x;");
-
-      //   ParseAssert.IsError(result);
-      //}
-
-      //[TestMethod]
-      //public void SeparatedBy_MultipleValues_ReturnsValues()
-      //{
-      //   var parser = Chars.Any().SeparatedBy(Chars.Char(';'));
-      //   var result = parser.Parse("x;y;z");
-
-      //   ParseAssert.ValueEquals(new char[] { 'x', 'y', 'z' }, result);
-      //}
-
-      //#endregion
    }
 }
--- ParsecSharp/Combine.cs
+++ ParsecSharp/Combine.cs
@@ -16,5 +16,53 @@ namespace PJanssen.ParsecSharp
       {
          return new OrParser<T>(parserA, parserB);
       }
+
+      /// <summary>
+      /// Applies the parser zero or more times.
+      /// </summary>
+      public static Parser<IEnumerable<T>> Many<T>(this Parser<T> parser)
+      {
+         Func<List<T>> seed = () => new List<T>();
+         Func<List<T>, T, List<T>> func = (xs, x) =>
+         {
+            xs.Add(x);
+            return xs;
+         };
+         Func<List<T>, IEnumerable<T>> sel = xs => (IEnumerable<T>)xs;
+
+         return parser.Aggregate(seed, func, sel);
+      }
+
+      /// <summary>
+      /// Applies the parser zero or more times.
+      /// </summary>
+      public static Parser<string> Many(this Parser<char> parser)
+      {
+         Func<StringBuilder> seed = () => new StringBuilder();
+         Func<StringBuilder, char, StringBuilder> func = (builder, c) => builder.Append(c);
+         Func<StringBuilder, string> sel = builder => builder.ToString();
+
+         return parser.Aggregate(seed, func, sel);
+      }
+
+      /// <summary>
+      /// Applies the parser one or more times.
+      /// </summary>
+      public static Parser<IEnumerable<T>> Many1<T>(this Parser<T> parser)
+      {
+         return from x in parser
+                from xs in Many(parser)
+                select new[] { x }.Concat(xs);
+      }
+
+      /// <summary>
+      /// Applies the parser one or more times.
+      /// </summary>
+      public static Parser<string> Many1(this Parser<char> parser)
+      {
+         return from x in parser
+                from xs in Many(parser)
+                select x + xs;
+      }
    }
 }
