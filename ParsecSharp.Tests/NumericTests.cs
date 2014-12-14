9fb9a0e15e69f44c22bc29b2e299275ab98313b0
Pier Janssen <pier@pjanssen.nl>
2014-12-14 18:57:48 +0100

    Added Numeric.Int

---
+++ ParsecSharp.Tests/NumericTests.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PJanssen.ParsecSharp
+{
+   [TestClass]
+   public class NumericTests
+   {
+      #region Int
+
+      [TestMethod]
+      public void Int_NoDigit_ReturnsError()
+      {
+         var parser = Numeric.Int();
+         var result = parser.Parse("xyz");
+
+         ParseAssert.IsError(result);
+      }
+
+      [TestMethod]
+      public void Int_Digits_ReturnsIntValue()
+      {
+         var parser = Numeric.Int();
+         var result = parser.Parse("42xyz");
+         
+         ParseAssert.ValueEquals(42, result);
+      }
+
+      [TestMethod]
+      public void Int_Negative_ReturnsIntValue()
+      {
+         var parser = Numeric.Int();
+         var result = parser.Parse("-42xyz");
+
+         ParseAssert.ValueEquals(-42, result);
+      }
+
+      [TestMethod]
+      public void Int_Plus_ReturnIntValue()
+      {
+         var parser = Numeric.Int();
+         var result = parser.Parse("+42xyz");
+
+         ParseAssert.ValueEquals(42, result);
+      }
+
+      #endregion
+   }
+}
