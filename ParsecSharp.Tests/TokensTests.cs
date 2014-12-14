b6cba19fb074e697887d2febf784c38a510c735c
Pier Janssen <pier@pjanssen.nl>
2014-12-14 19:06:05 +0100

    Added Tokens.Lexeme, Symbol

---
+++ ParsecSharp.Tests/TokensTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PJanssen.ParsecSharp
+{
+   [TestClass]
+   public class TokensTests
+   {
+      #region Lexeme
+
+      [TestMethod]
+      public void Lexeme_ConsumesTrailingWhitespace()
+      {
+         var parser = from x in Tokens.Lexeme(Chars.Char('x'))
+                      from y in Chars.Char('y')
+                      select x.ToString() + y.ToString();
+         var result = parser.Parse("x   \ty");
+
+         ParseAssert.ValueEquals("xy", result);
+      }
+
+      #endregion
+   }
+}
---
+++ ParsecSharp/Tokens.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PJanssen.ParsecSharp
+{
+   /// <summary>
+   /// Defines parsers for lexical elements.
+   /// </summary>
+   public static class Tokens
+   {
+      /// <summary>
+      /// Applies the given parser and consumes any following whitespace. Returns the parsed value.
+      /// </summary>
+      public static Parser<T> Lexeme<T>(Parser<T> parser)
+      {
+         return from value in parser
+                from _ in Chars.WhiteSpace().Many()
+                select value;
+      }
+
+      /// <summary>
+      /// Parses the given symbol name followed by any whitespace. Returns the parsed symbol name.
+      /// </summary>
+      public static Parser<string> Symbol(string name)
+      {
+         return Lexeme(Chars.String(name));
+      }
+   }
+}
