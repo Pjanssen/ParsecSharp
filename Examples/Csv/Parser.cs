4503aacd386bae19980227943e5b7ab330a7d284
Pier Janssen <pier@pjanssen.nl>
2015-02-07 15:57:22 +0100

    Added basic CSV parser example

---
+++ Examples/Csv/Parser.cs
@@ -0,0 +1,32 @@
+﻿using PJanssen.ParsecSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Csv
+{
+   public static class Parser
+   {
+      private const char _Separator = ',';
+      private const char _CR = '\r';
+      private const char _LF = '\n';
+
+      private static readonly Parser<char> ValueSeparator =
+            Chars.Char(_Separator);
+
+      private static readonly Parser<char> LineSeparator =
+            Chars.EndOfLine();
+
+      public static readonly Parser<string> Value = 
+            Chars.NoneOf(_Separator, _CR, _LF).Many();
+
+
+      public static readonly Parser<IEnumerable<string>> Line =
+            Value.SeparatedBy(ValueSeparator);
+
+
+      public static readonly Parser<IEnumerable<IEnumerable<string>>> Lines =
+            Line.SeparatedBy(LineSeparator);
+   }
+}
---
+++ Examples/Csv/Tests.cs
@@ -0,0 +1,75 @@
+﻿using Csv;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PJanssen.ParsecSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Csv
+{
+   [TestClass]
+   public class Tests
+   {
+      #region Value
+
+      [TestMethod]
+      public void CsvUnquotedValue_EmptyString()
+      {
+         var parser = Parser.Value;
+         var result = parser.Parse("");
+
+         ParseAssert.ValueEquals("", result);
+      }
+
+      [TestMethod]
+      public void CsvUnquotedValue()
+      {
+         var parser = Parser.Value;
+         var result = parser.Parse("test");
+
+         ParseAssert.ValueEquals("test", result);
+      }
+
+      #endregion
+
+      #region Line
+
+      [TestMethod]
+      public void CsvLine()
+      {
+         var parser = Parser.Line;
+         var result = parser.Parse("test,42,x");
+
+         ParseAssert.ValueEquals(new string[] { "test", "42", "x" }, result);
+      }
+
+      [TestMethod]
+      public void CsvLine_SeparatorsOnly()
+      {
+         var parser = Parser.Line;
+         var result = parser.Parse(",,");
+
+         ParseAssert.ValueEquals(new string[] { "", "", "" }, result);
+      }
+
+      #endregion
+
+      #region Lines
+
+      [TestMethod]
+      public void CsvLines()
+      {
+         var parser = Parser.Lines;
+         var result = parser.Parse("x,42\r\ny,84");
+
+         var row1 = result.FromSuccess().First().ToArray();
+         var row2 = result.FromSuccess().Skip(1).First().ToArray();
+
+         CollectionAssert.AreEqual(new string[] { "x", "42" }, row1);
+         CollectionAssert.AreEqual(new string[] { "y", "84" }, row2);
+      }
+
+      #endregion
+   }
+}
